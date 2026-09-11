1ac7da3eb2c0460b91a7c3d106fa916565bc5423
sanbei101 <sanbei101@outlook.com>
2026-09-12 00:08:00 +0800

    chore: 初始化 Next.js 脚手架，搭建基础工程

---
+++ next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactCompiler: true,
+  experimental: {
+    turbopackRustReactCompiler: true,
+  },
+  logging: {
+    incomingRequests: false,
+  },
+};
+
+export default nextConfig;
